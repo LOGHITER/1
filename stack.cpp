411a06388b22bc21e8b3e82ef5d5f15cc205452f
ivan <vanekplay235@gmail.com>
2024-03-21 15:37:16 +0300

    stack

---
+++ stack.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+struct Node {
+    Node* next = NIL; 
+    Node* prev = NIL;
+    Node* NIL;
+    int key = 0;
+};
+
+struct stack {
+    Node* top;
+    Node* bottom = top;
+    int size = 0;
+};
+
+stack* create_stack() {
+    stack* c = new stack;
+    return c;
+}
+
+bool empty(stack* stack) {
+    if(stack->size == 0) {
+        return true;
+    }
+    else {return false;}
+}
+
+void push(stack* stack, int key) {
+    if(empty(stack)) {
+        Node* c = new Node;
+        c->key = key;
+        c->next = c->NIL;
+        c->prev = c->NIL;
+        stack->top = c;
+    }
+    else{
+    Node* c = new Node;
+    c->next = stack->top;
+    c->key = key;
+    c->prev = c->NIL;
+    stack->top = c;
+    }
+    stack->size += 1;
+}
+
+Node* top(stack* stack) {
+    if(empty(stack)) {
+        return nullptr;
+    }   
+    return stack->top;
+}
+
+
+
+void pop(stack* stack) {
+    if(empty(stack)) {
+    }
+    else{
+        Node* c = stack->top->next;
+        delete stack->top;
+        stack->top = c;
+        stack->size -= 1;
+    }
+}
+
+int size(stack* stack) {
+    return stack->size;
+}
+
+int main() {
+    stack* s = create_stack();
+    push(s, 9);
+    push(s, 2);
+    std::cout << top(s) << std::endl;
+    pop(s);
+    std::cout << top(s);
+}
+
+
